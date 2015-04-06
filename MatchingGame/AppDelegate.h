6a9b2b72b3480ab80807e7f824e7d1543ce1e545
Graphic <graphic@SJH-611-04mac.local>
2015-04-06 16:49:11 -0400

    Initial Commit

---
+++ MatchingGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MatchingGame
+//
+//  Created by Graphic on 2015-04-06.
+//  Copyright (c) 2015 Min. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MatchingGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  MatchingGame
+//
+
+//  Copyright (c) 2015 Min. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
